cc9f72df298f69484c5e8f281e043150cccd9238
sonishreya13 <sonishreya8789@gmail.com>
2023-11-18 23:16:56 +0530

    Task4

---
+++ Task/dom.js
@@ -0,0 +1,12 @@
+let headerTitle= document.getElementById('header-title')
+let header= document.getElementById('main-header')
+header.style.borderBottom= 'black 3px solid';
+let addItem= document.getElementsByClassName('title')
+// console.log(addItem);
+addItem[0].style.color= "green";
+addItem[0].style.fontWeight= "bold";
+let groupList= document.getElementsByClassName('list-group-item')
+groupList[2].style.backgroundColor="green"
+for (let i = 0; i < groupList.length; i++) {
+    groupList[i].style.fontWeight = 'bold'; // Apply the style change to each element
+  }
--- Task3/dom.js
+++
@@ -1,7 +0,0 @@
-let headerTitle= document.getElementById('header-title')
-let header= document.getElementById('main-header')
-header.style.borderBottom= 'black 3px solid';
-let addItem= document.getElementsByClassName('title')
-// console.log(addItem);
-addItem[0].style.color= "green";
-addItem[0].style.fontWeight= "bold";
